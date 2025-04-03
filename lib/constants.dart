8ef74d671996811cd1fe68de5a523141edaba6d0
rejis22 <166278566+rejis22@users.noreply.github.com>
2025-04-03 10:35:16 +0530

    Add lib/constants.dart 1743656716268

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String appName = 'Zero2Robotics';
+  static const String ros2BridgeUrl = 'ws://your-ros2-bridge-url:9090';
+  static const String defaultMapLocation = 'assets/images/map_placeholder.png';
+}
